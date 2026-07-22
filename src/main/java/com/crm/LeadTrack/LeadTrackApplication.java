036f17da6851914cb2597c52e82762438e49291b
Vijay Kumar <vk6379183@gmail.com>
2026-07-22 19:38:47 +0530

    intial changes

---
+++ src/main/java/com/crm/LeadTrack/LeadTrackApplication.java
@@ -0,0 +1,13 @@
+package com.crm.LeadTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/LeadTrack/LeadTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.LeadTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
